f9fadad0e58671c89c55e12dfe28eae5b2434e69
Wim Van den Rul <wim.vandenrul@euri.com>
2017-01-06 16:55:40 +0100

    Deploy to staging script

--- deploy/deploy_staging.sh
+++ deploy/deploy_staging.sh
@@ -9,16 +9,30 @@ echo $CIRCLE_ARTIFACTS
 VERSION=$(node server/extractversion.js)
 echo $VERSION
 
-# Create TAG name (attach version)
-APP="registry.heroku.com/example123-staging"
-TAG=$VERSION
-echo $APP:$TAG
+REMOTE=wimvandenrul
+NAME=example123
 
 # Build docker image
-docker build -t $APP:$TAG .
-# Save docker image to artifacts folder in Circle CI
-docker save -o $CIRCLE_ARTIFACTS/example.tar $APP:$TAG
+docker build -t example123 .
+
+# TODO Save docker image to artifacts folder in Circle CI
+# docker save -o $CIRCLE_ARTIFACTS/example.tar $APP:$TAG
+
+# Tag docker image
+docker tag $NAME $REMOTE/$NAME:$VERSION
+# Push to Docker Hub
+docker push $REMOTE/$NAME:$VERSION
+# Tag docker image
+docker tag $NAME $REMOTE/$NAME:latest
+# Push to Docker Hub
+docker push $REMOTE/$NAME:latest
+
 # Login to Heroku repository
 docker login --email=_ --username=_ --password=$HEROKU_API_KEY registry.heroku.com
-# Push the docker image (deploy to Heroku)
-docker push $APP:$TAG
+
+# Deploy to Heroku
+REMOTE=registry.heroku.com
+NAME=example123
+
+docker tag $NAME registry.heroku.com/example123-staging/web
+docker push registry.heroku.com/example123-staging/web
